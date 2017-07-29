20c2e96fbc41459ea8ea9305205446a8f282dcfc
pandianvsb <30309127+pandianvsb@users.noreply.github.com>
2017-07-29 16:49:43 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int num;
+  printf("enter the number is odd or even");
+  scanf("%d",&num);
+  if(num%2==0)
+  {
+   printf("number is even\n");
+   }
+   else
+   {
+   printf("number is odd\n");
+   }
+   return 0;
+   }
+   
+  

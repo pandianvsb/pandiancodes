1ecac81dcda7ef7fc7bd66faf8c290e1e1f0d127
pandianvsb <30309127+pandianvsb@users.noreply.github.com>
2017-08-04 16:47:08 +0530

    Create eo.c

---
+++ eo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("enter the number is odd or even");
+  scanf("%d",&n);
+  if(n%2==0)
+  {
+   printf("number is even\n");
+   }
+   else
+   {
+   printf("number is odd\n");
+   }
+   return 0;
+}
